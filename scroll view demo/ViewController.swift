9a48f7600931af28cd468ebc9962415df424fae8
ehmatthes <eric@example.com>
2017-11-21 15:05:51 +0800

    init

--- scroll view demo/ViewController.swift
+++ scroll view demo/ViewController.swift
@@ -12,14 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        initViews()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initViews() {
+        
+        let scrollView = UIScrollView.init(frame: CGRect.init(x: -20, y: 0, width: view.bounds.width + 40, height: view.bounds.height))
+        scrollView.backgroundColor = randomColor()
+        view.addSubview(scrollView)
+        scrollView.isPagingEnabled = true
+        
+        /**
+         增加内容
+            */
+        for i in 0 ..< 4 {
+            let view = UIView.init(frame: CGRect.init(x: 20 + (self.view.bounds.width + 40) * CGFloat(i), y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+            view.backgroundColor = randomColor()
+            scrollView.addSubview(view)
+        }
+        
+        scrollView.contentSize = CGSize.init(width: (self.view.bounds.width + 40) * 4, height: self.view.bounds.height)
     }
-
+    
+    func randomColor() -> UIColor {
+        return UIColor.init(red: CGFloat(arc4random_uniform(UInt32(255.0))) / CGFloat(255), green: CGFloat(arc4random_uniform(UInt32(255.0))) / CGFloat(255), blue: CGFloat(arc4random_uniform(UInt32(255.0))) / CGFloat(255), alpha: 1)
+    }
 
 }
 
